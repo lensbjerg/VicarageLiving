5aa32278ed3706110c006ccc6ba1492b32f9c1e3
Christian F. Lensbjerg <lensbjerg@gmail.com>
2026-02-24 16:37:33 +0100

    logo, season images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-home',
-    imports: [TranslocoModule],
+    imports: [TranslocoModule, UpperCasePipe],
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
