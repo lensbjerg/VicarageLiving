76035f0f2633326860a5a28eff0dcf574d7913aa
Christian F. Lensbjerg <lensbjerg@gmail.com>
2026-02-24 13:30:02 +0100

    bfvb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+
+
+// constructor(router: Router) {
+//   router.events
+//     .pipe(filter(e => e instanceof NavigationEnd))
+//     .subscribe(() => {
+//       (window as any).umami?.track();
+//     });
+// }
